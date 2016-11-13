88df6a802385ae6329e6f9c35ecc6dca9231d015
Viktor Puselja <viktor.puselja@gmail.com>
2016-11-13 23:47:56 +0100

    Add route for note-form-page

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 
 const notes = require('./routes/notes');
 const users = require('./routes/users');
+const noteForm = require('./routes/note-form');
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', notes);
 app.use('/users', users);
+app.use('/note-form', noteForm);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
