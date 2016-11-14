0b85feb8075133d2f2d63ec55a7fa7d077b8be78
Viktor Puselja <viktor.puselja@gmail.com>
2016-11-14 10:59:44 +0100

    Turn dummy notes view into template

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,23 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('notes', {title: 'Express'});
+    res.render('notes', {
+        notes: [
+            {
+                dueSentence: "",
+                title: "Einkaufen",
+                importanceStars: "*****",
+                finishedChecked: "checked",
+                description: "Pizza\nSchuhe\nSuesses",
+            }, {
+                dueSentence: "in a month",
+                title: "Geburt von Sarah",
+                importanceStars: "***",
+                finishedChecked: "",
+                description: "Anrufen",
+            },
+        ],
+    });
 });
 
 module.exports = router;
