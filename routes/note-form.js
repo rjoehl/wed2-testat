5a4e1407b6787ab783de33a793ee06cb0343ae16
Viktor Puselja <viktor.puselja@gmail.com>
2016-11-14 18:12:32 +0100

    Implement page titles

--- routes/note-form.js
+++ routes/note-form.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.render('note-form');
+    res.render('note-form', {
+        title: "Edit note",
+    });
 });
 
 module.exports = router;
