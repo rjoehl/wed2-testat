87d802e5c69a9407c9ccef95e973a046eb5b9991
Viktor Puselja <viktor.puselja@gmail.com>
2016-11-14 10:30:28 +0100

    Reformat code

--- routes/note-form.js
+++ routes/note-form.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('note-form');
+router.get('/', function (req, res, next) {
+    res.render('note-form');
 });
 
 module.exports = router;
--- routes/notes.js
+++ routes/notes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('notes', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('notes', {title: 'Express'});
 });
 
 module.exports = router;
