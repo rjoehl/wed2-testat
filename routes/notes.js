529ecdb3a049806cbc9e18c4413008b3ba26558c
Viktor Puselja <viktor.puselja@gmail.com>
2016-11-16 11:08:45 +0100

    Implement show-finished button

--- routes/notes.js
+++ routes/notes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 let isAlternativeStyle = false;
+let showFinished = false;
 
 const Params = {
-    ALTERNATIVE_STYLE: 'alternative-style'
+    ALTERNATIVE_STYLE: 'alternative-style',
+    SHOW_FINISHED: 'show-finished'
 };
 
 /* GET home page. */
@@ -18,12 +20,26 @@ router.get('/', function (req, res, next) {
         }
     }
 
+    if (req.query.hasOwnProperty(Params.SHOW_FINISHED)) {
+        const value = req.query[Params.SHOW_FINISHED];
+        if (value === 'true') {
+            showFinished = true;
+        } else if (value === 'false') {
+            showFinished = false;
+        }
+    }
+
     res.render('notes', {
         title: 'Notes',
         style: isAlternativeStyle ? 'alternative' : '',
         alternativeStyle: {
             value: !isAlternativeStyle,
             class: isAlternativeStyle ? 'active' : ''
+        },
+        showFinished: {
+            value: !showFinished,
+            class: showFinished ? 'active' : '',
+            text: !showFinished ? 'Show finished' : 'Hide finished'
         },
         notes: [
             {
