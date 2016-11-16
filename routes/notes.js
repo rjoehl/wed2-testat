14c1a2d419e5f49b10751a121af7484b8ae9e9ac
Viktor Puselja <viktor.puselja@gmail.com>
2016-11-16 12:22:50 +0100

    Remove obsolete comments

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const notesController = require('../controllers/notes');
 
-/* GET home page. */
 router.get('/', notesController);
 
 module.exports = router;
