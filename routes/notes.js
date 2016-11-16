74363691a845e44daf0d517466672f73c3952d1c
Viktor Puselja <viktor.puselja@gmail.com>
2016-11-16 10:04:08 +0100

    Replace double with single quotes in code

--- routes/notes.js
+++ routes/notes.js
@@ -3,31 +3,31 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    const isAlternativeStyle = req.query.style === "alternative";
+    const isAlternativeStyle = req.query.style === 'alternative';
     res.render('notes', {
-        title: "Notes",
+        title: 'Notes',
         style: getStyle(isAlternativeStyle),
         otherStyle: getStyle(!isAlternativeStyle),
         notes: [
             {
-                dueSentence: "",
-                title: "Einkaufen",
-                importanceStars: "*****",
-                finishedChecked: "checked",
-                description: "Pizza\nSchuhe\nSuesses"
+                dueSentence: '',
+                title: 'Einkaufen',
+                importanceStars: '*****',
+                finishedChecked: 'checked',
+                description: 'Pizza\nSchuhe\nSuesses'
             }, {
-                dueSentence: "in a month",
-                title: "Geburt von Sarah",
-                importanceStars: "***",
-                finishedChecked: "",
-                description: "Anrufen"
+                dueSentence: 'in a month',
+                title: 'Geburt von Sarah',
+                importanceStars: '***',
+                finishedChecked: '',
+                description: 'Anrufen'
             }
         ]
     });
 });
 
 function getStyle(isAlternativeStyle) {
-    return isAlternativeStyle ? "alternative" : "standard";
+    return isAlternativeStyle ? 'alternative' : 'standard';
 }
 
 module.exports = router;
