86701b09e50762e929362a96c9d2118ffeaf6873
Raphael <raphael.joehl@outlook.com>
2016-11-17 13:15:01 +0100

    Implement alternative style for whole site

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+    const isAlternativeStyle = req.session.isAlternativeStyle;
+    res.locals.style = isAlternativeStyle ? 'alternative' : 'standard';
+    next();
+});
+
 app.use('/', notes);
 
 // catch 404 and forward to error handler
